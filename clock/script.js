107ec4578cec6f115266c9d189802d69dfc2c34d
dduskin <dpduskin@gmail.com>
2020-03-21 06:02:50 -0500

    put milli back

--- clock/script.js
+++ clock/script.js
@@ -7,7 +7,7 @@ Number.prototype.pad = function(n) {
 
 function updateClock() {
   var now = new Date();
-  // var milli = now.getMilliseconds(),
+  var milli = now.getMilliseconds(),
     sec = now.getSeconds(),
     min = now.getMinutes(),
     hou = now.getHours(),
@@ -16,9 +16,7 @@ function updateClock() {
     yr = now.getFullYear();
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
-    corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2)
-    // , milli
-  ];
+    corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
   for (var i = 0; i < tags.length; i++)
     document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
 }
